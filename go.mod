c9d4c02ba36d929abc6d250a584a81bf1eb43729
Nic Johnson <nicjohnson145@hotmail.com>
2022-08-06 19:44:37 -0500

    feat: initial cut

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/nicjohnson145/strugen
+
+go 1.18
+
+require (
+	github.com/fatih/structtag v1.2.0
+	github.com/samber/lo v1.27.0
+	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/tools v0.1.12
+)
+
+require (
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+)
---
+++ strugen.go
@@ -0,0 +1,126 @@
+package strugen
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+
+	"github.com/fatih/structtag"
+	"go/printer"
+	"go/token"
+	"strings"
+
+	"github.com/samber/lo"
+	"golang.org/x/tools/go/packages"
+)
+
+type Generator struct {
+	Types   []string
+	TagName string
+}
+
+type Struct struct {
+	Name   string
+	Fields map[string]StructField
+}
+
+type StructField struct {
+	Name     string
+	Exported string
+	Tagged   bool
+	TagValue string
+}
+
+func (g *Generator) FindStructs() (map[string]Struct, error) {
+	cfg := &packages.Config{
+		Mode:  packages.LoadSyntax,
+		Tests: false,
+	}
+	pkgs, err := packages.Load(cfg, ".")
+	if err != nil {
+		return map[string]Struct{}, fmt.Errorf("error loading package: %v", err)
+	}
+
+	if len(pkgs) != 1 {
+		return map[string]Struct{}, fmt.Errorf("error: %d packages found, expected 1", len(pkgs))
+	}
+
+	structs := map[string]Struct{}
+
+	for _, f := range pkgs[0].Syntax {
+		smap, err := g.parseStruct(f, pkgs[0].Fset)
+		if err != nil {
+			return map[string]Struct{}, err
+		}
+
+		structs = lo.Assign(structs, smap)
+	}
+
+	return structs, nil
+}
+
+func (g *Generator) parseStruct(file *ast.File, fileSet *token.FileSet) (map[string]Struct, error) {
+	sMap := map[string]Struct{}
+
+	var inspectError error
+
+	ast.Inspect(file, func(n ast.Node) bool {
+		// If we've errored on a previous node, then just stop trying
+		if inspectError != nil {
+			return false
+		}
+
+		typeSpec, ok := n.(*ast.TypeSpec)
+		if !ok || typeSpec.Type == nil {
+			return true
+		}
+
+		s, ok := typeSpec.Type.(*ast.StructType)
+		if !ok {
+			return true
+		}
+
+		struct_ := Struct{
+			Name: typeSpec.Name.Name,
+			Fields: map[string]StructField{},
+		}
+		for _, field := range s.Fields.List {
+			sf := StructField{
+				Name: field.Names[0].Name,
+			}
+			typeNameBuf := new(bytes.Buffer)
+			err := printer.Fprint(typeNameBuf, fileSet, field.Type)
+			if err != nil {
+				inspectError = fmt.Errorf("error printing type name: %v", err)
+				return false
+			}
+
+			if field.Tag != nil {
+				tag := field.Tag.Value
+				tag = strings.Trim(tag, "`")
+				tags, err := structtag.Parse(tag)
+				if err != nil {
+					inspectError = fmt.Errorf("error extracting tags: %v", err)
+					return false
+				}
+
+				userTag, err := tags.Get(g.TagName)
+				if err == nil {
+					sf.Tagged = true
+					sf.TagValue = strings.Join(append([]string{userTag.Name}, userTag.Options...), ",")
+				} else {
+					sf.Tagged = false
+				}
+			} else {
+				sf.Tagged = false
+			}
+
+			struct_.Fields[sf.Name] = sf
+		}
+
+		sMap[struct_.Name] = struct_
+		return false
+	})
+
+	return sMap, inspectError
+}
